6c66b28d7e4d596b37454b5243b78864fde483e6
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 18:56:53 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,25 +1,5 @@
 require_relative "./vehicle.rb"
 
-class Vehicle
-  
-  attr_accessor :wheel_size, :wheel_number
-  @@all = []
-  
-  def initialize(wheel_size,wheel_number)
-    @wheel_size = wheel_size
-    @wheel_number = wheel_number
-    @@all << self
-  end
-  
-  def go 
-    "vrrrrrrrooom!"
-  end
-  
-  def fill_up_tank
-    "filling up!"
-  end
-end
-
 class Car < Vehicle
   
   def go 
