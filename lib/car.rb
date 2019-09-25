5000bbd1dd51972f39ddf6cbcec10afa3a3c61aa
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-25 18:49:56 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,16 @@
 require_relative "./vehicle.rb"
+
+class Car
+  
+  attr_accessor
+  @@all = []
+  
+  def initialize(wheel_size,number)
+    @wheel_size = wheel_size
+    @numb = number
+    @@all << self
+  end
+  
+  
+  
+end
